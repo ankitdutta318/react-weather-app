c88f9bb6c7777ecb2bfd79c69fde71a78a071c2b
cruxbreaker <geekankit318@gmail.com>
2018-05-17 18:53:35 +0530

    Added basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = "591466f240ccdef0581e14835732bc6d";
+
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=jamshedpur&appid=${API_KEY}&unit=metric`);
+    const data = await api_call.json();
+
+    console.log(data);
+  };
   render() {
     return (
       <div>
-        <h1>Hello! </h1>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
