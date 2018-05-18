0733a086a65cf6fae2c264b844bcc452b51f7fae
cruxbreaker <geekankit318@gmail.com>
2018-05-18 14:08:18 +0530

    Passing variables values by props and accessing it via getState()

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
-const API_KEY = "591466f240ccdef0581e14835732bc6d";
+const API_KEY = "591466f240ccdef0581e14835732bc6d";   // App API key
 
 class App extends React.Component {
-  getWeather = async (e) => {
-    e.preventDefault();
 
+  // Set default state of variables
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    temp_max: undefined,
+    temp_min: undefined,
+    wind: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  
+  // Function to fetch weather from the OpenMap API
+  getWeather = async (e) => {
+    e.preventDefault();   // Prevent default behaviour of complete page refresh
+
+    // Form variables
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
@@ -17,13 +33,35 @@ class App extends React.Component {
     const data = await api_call.json();
 
     console.log(data);
+
+    // Set new state of the variables
+    this.setState({
+      city: data.name,
+      country: data.sys.country,
+      temperature: data.main.temp,
+      temp_max: data.main.temp_max,
+      temp_min: data.main.temp_min,
+      wind: data.wind.speed,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   };
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+        city={this.state.city}
+        country={this.state.country}
+        temperature={this.state.temperature}
+        temp_max={this.state.temp_max}
+        temp_min={this.state.temp_min}
+        wind={this.state.wind}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,13 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                Weather Component
+                { this.props.city && this.props.country && <p>city: { this.props.city }, { this.props.country }</p> }
+                { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+                { this.props.temp_max && <p>temp_max: { this.props.temp_max }</p> }
+                { this.props.temp_min && <p>temp_min: { this.props.temp_min }</p> }
+                { this.props.wind && <p>wind: { this.props.wind }</p> }
+                { this.props.humidity && <p>humidity: { this.props.humidity }</p> }
+                { this.props.description && <p>description: { this.props.description }</p> }
             </div>
         );
     }
